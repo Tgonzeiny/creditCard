c255423ea197df5a9311716be6fa470675557f33
Tgonzeiny <152939588+Tgonzeiny@users.noreply.github.com>
2025-05-20 21:09:00 -0400

    Worked on flask app

--- backend/flaskApp.py
+++ backend/flaskApp.py
@@ -1,7 +1,20 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from models import userModel, cardModel, rewrardModel
+from models import userModel, cardModel, rewardModel
 
 
-flaskApp = flask(__name__)
-CORS(flaskApp)
+flaskApp = Flask(__name__)
+CORS(flaskApp) #allows the app to abe called from other sources
+
+@flaskApp.route("/")
+def home():
+    return jsonify({"message": "Welcome to creditCardAPI"})
+
+#added later
+#addUser
+#getUserCards
+#getCardRewards
+#addCardToUser
+
+if __name__ == "__main__":
+    flaskApp.run(debug=True)
