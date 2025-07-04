3d867d4bb1bbe12d7eb4190cbfb5515da51990c6
Thomas Gonzalez <tgonzeiny@gmail.com>
2025-07-04 14:57:05 -0400

    testing the pipeline

--- backend/tests/testCreateUser.py
+++ backend/tests/testCreateUser.py
@@ -2,14 +2,14 @@ import pytest
 from backend.accounts.userAccounts import userAccounts
 
 #Tests if an user can be made
-def testCreateUser():
+def test_Create_User():
     user = userAccounts()
     response = user.createUser("TestUser", "test@example.com", "test123")
     assert response["success"] is True or response["message"] == "User created successfully"
     user.close()
 
 #Tests if user can be made if email exits
-def testDCreateDuplicateUser():
+def test_Create_Duplicate_User():
     user = userAccounts()
     user.createUser("TestUser", "test@example.com", "test123")
     result = user.createUser("TestUser", "test@example.com", "test123")
