9f7ae690f782ba1cc4e7e9b03b0607398b47f531
Thomas Gonzalez <tgonzeiny@gmail.com>
2025-08-16 14:00:40 -0400

    fix

--- backend/tests/test_password_hash.py
+++ backend/tests/test_password_hash.py
@@ -3,16 +3,15 @@ from backend.security.passwordHandler import PasswordHandler
 
 def test_password_hashing():
     password = "testPassword123"
-    handler = PasswordHandler()
 
     # Hash the password
-    hashed_password = handler.hash_password(password)
+    hashed_password = PasswordHandler.hash_password(password)
     assert hashed_password is not None
 
     # Verify the password
-    is_correct = handler.check_password(password, hashed_password)
+    is_correct = PasswordHandler.check_password(password, hashed_password)
     assert is_correct == True
 
     # Test with an incorrect password
-    is_incorrect = handler.check_password("wrongPassword", hashed_password)
+    is_incorrect = PasswordHandler.check_password(hashed_password, "wrongPassword")
     assert is_incorrect == False
